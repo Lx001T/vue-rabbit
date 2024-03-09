0fc0573f88a8783d235ddbb92d9afeeae34f1d3b
L jq <2609193840@qq.com>
2024-03-09 11:03:50 +0800

    scss文件的自动导入

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+
+// 引入初始化样式文件
+import '@/styles/common.scss'
 
 
 import { getCategory } from '@/apis/testAPI'
@@ -12,6 +13,7 @@ getCategory().then(res => {
   console.log(res);
 })
 const app = createApp(App)
+
 
 app.use(createPinia())
 app.use(router)
