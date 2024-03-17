d68d2576cb40b5640980b011ac9e29ace71e2955
L jq <2609193840@qq.com>
2024-03-17 16:45:32 +0800

    home-懒加载优化

---
+++ src/directives/index.js
@@ -0,0 +1,27 @@
+// 定义懒加载插件
+import { useIntersectionObserver } from '@vueuse/core'
+
+export const lazyPlugin = {
+  install (app) {
+    // 懒加载逻辑
+    app.directive('img-lazy', {
+      mounted(el, binding) {
+        // el：绑定哪个元素 img
+        // binding：bingding.value 指令等于号后面绑定的表达式的值 图片url
+        console.log(el, binding.value);
+        const {stop} = useIntersectionObserver(
+          el,
+          ([{ isIntersecting }]) => {
+            console.log(isIntersecting);
+            if (isIntersecting) {
+              // 进入视图区域
+              el.src = binding.value
+              stop()
+            }
+          },
+        )
+      }
+    })
+    
+  }
+}
--- src/main.js
+++ src/main.js
@@ -7,36 +7,13 @@ import router from './router'
 // 引入初始化样式文件
 import '@/styles/common.scss'
 
-// // 测试接口函数
-// import { getCategory } from './apis/testAPI'
-// getCategory().then(res => {
-//   console.log(res);
-// })
-
-import { useIntersectionObserver } from '@vueuse/core'
+// 引入懒加载指令并且注册
+import { lazyPlugin } from '@/directives/index'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(lazyPlugin)
 
 app.mount('#app')
-
-// 定义全局指令
-app.directive('img-lazy', {
-  mounted(el, binding) {
-    // el：绑定哪个元素 img
-    // binding：bingding.value 指令等于号后面绑定的表达式的值 图片url
-    console.log(el, binding.value);
-    useIntersectionObserver(
-      el,
-      ([{ isIntersecting }]) => {
-        console.log(isIntersecting);
-        if (isIntersecting) {
-          // 进入视图区域
-          el.src = binding.value
-        }
-      },
-    )
-  }
-})
