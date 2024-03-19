77fb98a2265ba712f3d8b469223227e225d0fbd0
L jq <2609193840@qq.com>
2024-03-19 16:57:29 +0800

    导航激活设置分类列表渲染

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -1,8 +1,9 @@
 <script setup>
-import {getCategoryAPI} from '@/apis/category'
-import {onMounted, ref} from 'vue'
+import { getCategoryAPI } from '@/apis/category'
+import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
 import { getBannerAPI } from '@/apis/home';
+import GoodsItem from '../Home/components/GoodsItem.vue';
 
 // 获取数据
 const categoryData = ref({})
@@ -34,18 +35,36 @@ onMounted(() => getBanner())
       <div class="bread-container">
         <el-breadcrumb separator=">">
           <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-          <el-breadcrumb-item>{{categoryData.name}}</el-breadcrumb-item>
+          <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
         </el-breadcrumb>
       </div>
       <!-- 轮播图 -->
       <div class="home-banner">
-    <el-carousel height="500px">
-      <el-carousel-item v-for="item in bannerList" :key="item.id">
-        <img 
-          :src="item.imgUrl" alt="">
-      </el-carousel-item>
-    </el-carousel>
-  </div>
+        <el-carousel height="500px">
+          <el-carousel-item v-for="item in bannerList" :key="item.id">
+            <img :src="item.imgUrl" alt="">
+          </el-carousel-item>
+        </el-carousel>
+      </div>
+      <div class="sub-list">
+        <h3>全部分类</h3>
+        <ul>
+          <li v-for="i in categoryData.children" :key="i.id">
+            <RouterLink to="/">
+              <img :src="i.picture" />
+              <p>{{ i.name }}</p>
+            </RouterLink>
+          </li>
+        </ul>
+      </div>
+      <div class="ref-goods" v-for="item in categoryData.children" :key="item.id">
+        <div class="head">
+          <h3>- {{ item.name }}-</h3>
+        </div>
+        <div class="body">
+          <GoodsItem v-for="good in item.goods" :good="good" :key="good.id" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -130,15 +149,15 @@ onMounted(() => getBanner())
   }
 
   .home-banner {
-  width: 1240px;
-  height: 500px;
-  margin: 0 auto;
-  z-index: 98;
-
-  img {
-    width: 100%;
+    width: 1240px;
     height: 500px;
+    margin: 0 auto;
+    z-index: 98;
+
+    img {
+      width: 100%;
+      height: 500px;
+    }
   }
-}
 }
 </style>
