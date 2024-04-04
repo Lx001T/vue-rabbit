df3c28c44596c4e702f630c6af33bf063213aa7c
L jq <2609193840@qq.com>
2024-04-04 14:50:06 +0800

    二级分类-列表无限加载实现

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { getCategoryFilterAPI,getSubCategoryAPI } from '@/apis/category'
+import { getCategoryFilterAPI, getSubCategoryAPI } from '@/apis/category'
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
 import GoodsItem from '../Home/components/GoodsItem.vue';
@@ -17,8 +17,8 @@ onMounted(() => getCategoryData())
 const goodList = ref({})
 const reqData = ref({
   categoryId: route.params.id,
-  page:1,
-  pageSize:20,
+  page: 1,
+  pageSize: 20,
   sortField: 'publishTime'
 })
 const getGoodList = async () => {
@@ -26,6 +26,27 @@ const getGoodList = async () => {
   goodList.value = res.result.items
 }
 onMounted(() => getGoodList())
+
+// tab切换回调
+const tabChange = () => {
+  console.log('切换了', reqData.value.sortField)
+  reqData.value.page = 1
+  getGoodList()
+}
+
+// 加载风多
+const disabled = ref(false)
+const load = async () => {
+  console.log('加载风多数据');
+  // 获取下一页数据
+  reqData.value.page++
+  const res = await getSubCategoryAPI(reqData.value)
+  goodList.value = [...goodList.value, ...res.result.items]
+  // 加载完毕，停止监听
+  if (res.result.items.length == 0) {
+    disabled.value = true
+  }
+}
 </script>
 
 <template>
@@ -34,20 +55,20 @@ onMounted(() => getGoodList())
     <div class="bread-container">
       <el-breadcrumb separator=">">
         <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-        <el-breadcrumb-item :to="{ path: `/category/${categoryData.parentId}` }">{{categoryData.parentName}}
+        <el-breadcrumb-item :to="{ path: `/category/${categoryData.parentId}` }">{{ categoryData.parentName }}
         </el-breadcrumb-item>
-        <el-breadcrumb-item>{{categoryData.name}}</el-breadcrumb-item>
+        <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
       </el-breadcrumb>
     </div>
     <div class="sub-container">
-      <el-tabs>
+      <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
-      <div class="body">
-         <!-- 商品列表-->
-         <GoodsItem v-for="good in goodList" :good="good" :key="good.id"/>
+      <div class="body" v-infinite-scroll="load" :infinite-scroll-disabled="disabled">
+        <!-- 商品列表-->
+        <GoodsItem v-for="good in goodList" :good="good" :key="good.id" />
       </div>
     </div>
   </div>
