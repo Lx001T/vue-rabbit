8c7707a5dbd53c9a7ee2d17b5151dfecb1a06957
L jq <2609193840@qq.com>
2024-04-07 11:46:27 +0800

    登录-pinia数据持久化与token配置多模板显示

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
@@ -11,8 +12,11 @@ import '@/styles/common.scss'
 import { lazyPlugin } from '@/directives/index'
 
 const app = createApp(App)
+const pinia = createPinia()
+// 注册持久化插件
+pinia.use(piniaPluginPersistedstate)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 app.use(lazyPlugin)
 
--- src/stores/user.js
+++ src/stores/user.js
@@ -1,14 +1,14 @@
 // 管理用户数据相关
 import { loginAPI } from '@/apis/user'
-import {defineStore} from 'pinia'
+import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
-export const useUserStore = defineStore('user', ()=> {
+export const useUserStore = defineStore('user', () => {
   // 1. 定义管理用户数据的state
   const userInfo = ref({})
   // 2.定义获取接口数据的action函数
-  const getUserInfo = async ({account, password}) => {
-    const res = await loginAPI({account, password})
+  const getUserInfo = async ({ account, password }) => {
+    const res = await loginAPI({ account, password })
     userInfo.value = res.result
   }
   // 3。以对象的格式把state和action return
@@ -16,4 +16,7 @@ export const useUserStore = defineStore('user', ()=> {
     userInfo,
     getUserInfo
   }
-})
+},
+  {
+    persist: true,
+  })
